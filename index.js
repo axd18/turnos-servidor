12691032b8a036515ed8e31164e986dbb699caf4
adrian <adrianfernandezj@gmail.com>
2020-08-13 18:54:00 -0300

    Creando las API

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// crear el servidor
+const app = express();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+
+
+// arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
