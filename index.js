abe5a0a8bdf07bead5699ea627a6ee7f5cb4dd4d
adrian <adrianfernandezj@gmail.com>
2020-08-14 19:12:29 -0300

    setup DB

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // crear el servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
+
 // puerto de la app
 const PORT = process.env.PORT || 4000;
 
-
+// Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'));
 
 // arrancar la app
 app.listen(PORT, () => {
---
+++ routes/usuarios.js
@@ -0,0 +1,10 @@
+// Rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+
+// Cre un usuario
+// api/usuarios
+router.post('/', () => {
+
+});
+module.exports = router;
