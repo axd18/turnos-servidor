f48af40c238c03cd4a5cba3ef8479cb2b1d14f8c
adrian <adrianfernandezj@gmail.com>
2020-08-21 07:20:37 -0300

    267 Obteniendo información del usuario autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -53,3 +53,14 @@ exports.autenticarUsuario = async (req, res) => {
 
    
 }
+
+// Obtiene que usuario está autenticado
+exports.usuarioAutenticado = async (req, res) => {
+   try {
+      const usuario = await Usuario.findById(req.usuario.id);
+      res.json({usuario});  
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({msg: 'Hubo un error'});
+   }
+}
